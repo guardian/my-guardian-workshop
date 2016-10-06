240f74954fd40685ee28921d564a22ac46ca2605
Philip McMahon <philip.mcmahon@guardian.co.uk>
2016-10-06 12:32:53 +0100

    Update comments, simplify api calls.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function getInterestingFields(contentItem){
 
 function addRowOfNews(rowSubject, numberOfItems = 3){
     var contentApiQuery = encodeURIComponent(rowSubject);
-    return addContentList(rowSubject, "http://content.guardianapis.com/search?show-fields=all&show-elements=all&q="+contentApiQuery, numberOfItems);
+    return addContentList(rowSubject, "http://content.guardianapis.com/search?q="+contentApiQuery, numberOfItems);
 }
 
 function makeContentApiUrlFromGuardianUrl(guardianUrl) {
@@ -18,7 +18,8 @@ function makeContentApiUrlFromGuardianUrl(guardianUrl) {
     return contentApiURL + "?show-fields=all&show-elements=all"
 }
 
-// This adds the top 3 articles from a content api search result. Try changing the URL to something else
+// This adds the top 3 articles from a content api search. Try changing "Harry Potter" to something else.
+// You could also add an extra row of news by copying and pasting this line - give it a go!
 addRowOfNews("Harry Potter", 3)
 
 // we sleep (pause) here to make sure that everything is added in the right order
@@ -26,7 +27,7 @@ sleep(500);
 
 // Here, you can choose specific pieces of content to add to your page. 
 addContentItems("Selected News", [
-    "https://content.guardianapis.com/lifeandstyle/2015/oct/02/are-these-the-10-worst-board-games-of-all-time?show-fields=all&show-elements=all",
-    "https://content.guardianapis.com/education/2016/mar/27/tom-bennett-behaviour-schools-teaching-hunger-games?show-fields=all&show-elements=all",
-    "https://content.guardianapis.com/lifeandstyle/womens-blog/2016/mar/03/my-great-grandmother-emmeline-pankhurst-would-still-be-fighting-for-equality-today?show-fields=all&show-elements=all"
+    "https://content.guardianapis.com/lifeandstyle/2015/oct/02/are-these-the-10-worst-board-games-of-all-time?",
+    "https://content.guardianapis.com/education/2016/mar/27/tom-bennett-behaviour-schools-teaching-hunger-games?",
+    "https://content.guardianapis.com/lifeandstyle/womens-blog/2016/mar/03/my-great-grandmother-emmeline-pankhurst-would-still-be-fighting-for-equality-today?"
     ])
