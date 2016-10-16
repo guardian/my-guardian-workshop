d20ef57313fda8e02a6ca71e0df77d99ffe127cb
Philip McMahon <philip.mcmahon@guardian.co.uk>
2016-10-16 20:29:29 +0200

    More interesting finished changes

--- script.js
+++ script.js
@@ -21,14 +21,21 @@ function makeContentApiUrlFromGuardianUrl(guardianUrl) {
 
 // This adds the top 3 articles from a content api search. Try changing "Harry Potter" to something else.
 // You could also add an extra row of news by copying and pasting this line - give it a go!
-addRowOfNews("Harry Potter", 3)
+addRowOfNews("Inspirational Women", 3)
 
 // we sleep (pause) here to make sure that everything is added in the right order
 sleep(500);
 
+addRowOfNews("Facebook", 3)
+
+
+
+
+
+
 // Here, you can choose specific pieces of content to add to your page. 
-addContentItems("Selected News", [
-    "https://content.guardianapis.com/lifeandstyle/2015/oct/02/are-these-the-10-worst-board-games-of-all-time",
-    "https://content.guardianapis.com/education/2016/mar/27/tom-bennett-behaviour-schools-teaching-hunger-games",
-    "https://content.guardianapis.com/lifeandstyle/womens-blog/2016/mar/03/my-great-grandmother-emmeline-pankhurst-would-still-be-fighting-for-equality-today"
-    ])
+// addContentItems("Selected News", [
+//     "https://content.guardianapis.com/lifeandstyle/2015/oct/02/are-these-the-10-worst-board-games-of-all-time",
+//     "https://content.guardianapis.com/education/2016/mar/27/tom-bennett-behaviour-schools-teaching-hunger-games",
+//     "https://content.guardianapis.com/lifeandstyle/womens-blog/2016/mar/03/my-great-grandmother-emmeline-pankhurst-would-still-be-fighting-for-equality-today"
+//     ])
