f1d4066359865ca3e62acbcc0c124cb5da9ecccd
Philip McMahon <philip.mcmahon@guardian.co.uk>
2018-10-10 11:54:20 +0100

    Improve documentation ofx upload site script

--- upload-site.sh
+++ upload-site.sh
@@ -4,9 +4,11 @@
 export AWS_ACCESS_KEY_ID=<aws-access-key>
 export AWS_SECRET_ACCESS_KEY=<aws-secret-key>
 
+BUCKET_NAME=ada-lovelace-day
+
 if [ $# -ne 1 ]
 then
-    echo "Enter your full name all lowercase with no spaces (you can use hyphens instead) after upload-site.sh"
+    echo "Enter your full name all lowercase with no spaces (you can use hyphens instead) after upload-site.sh, for example: `./upload-site.sh elonmusk` "
     exit
 fi
 
@@ -14,5 +16,10 @@ fi
 # on most machines with the aws cli installed you should be able to get away with just 'aws'
 /Library/Frameworks/Python.framework/Versions/2.7/bin/aws s3 sync . s3://ada-lovelace-day/$1 --exclude '*' --include '*.html' --include '*.js' --include '*.css' --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 
-echo "-------------------------------"
-echo "Your site is at http://ada-lovelace-day.s3-website-eu-west-1.amazonaws.com/$1"
+if [ $? -ne 0 ]
+then
+   echo "Upload failed"
+else
+   echo "-------------------------------"
+   echo "Your site is at http://$BUCKET_NAME.s3-website-eu-west-1.amazonaws.com/$1"
+fi
